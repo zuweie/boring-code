97c93b59b53187f2082a7c195f06fafe3b6c40b3
zuweie <jojoe.wei@gmail.com>
2020-12-31 23:27:37 +0800

    fourier transform

---
+++ src/fourier_transform/fourier_transform.c
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-29 16:12:36
+ * @LastEditTime: 2020-12-30 16:23:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /boring-code/src/fourier_transform/fourier_transform.c
+ */
+#include <math.h>
+#include "complex/complex.h"
+#include "fourier_transform.h"
+
+/**
+ * 离散傅立叶变换
+ * squence 采样数据，大小一般为 2^x 大小
+ * out 傅立叶变换输出的复数
+*/
+int discrete_fourier_transform (float squence[], size_t squence_size, complex_t out[]) 
+{
+    
+}
+
+int fast_fourier_transform(void) 
+{
+
+}
--- src/fourier_transform/fourier_transform.h
+++ src/fourier_transform/fourier_transform.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-29 16:14:16
- * @LastEditTime: 2020-12-29 16:19:36
+ * @LastEditTime: 2020-12-30 16:17:41
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /boring-code/src/fourier_transform/fourier_transform.h
@@ -9,7 +9,9 @@
 
 #ifndef _FOURIER_TRANSFORM_
 #define _FOURIER_TRANSFROM_
+#include "complex/complex.h"
 
-int discrete_fourier_transform (void);
+int discrete_fourier_transform (float[], size_t, complex_t[]);
 int fast_fourier_transform(void);
+
 #endif
