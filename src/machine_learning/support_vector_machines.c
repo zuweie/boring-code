5fc1d5edfca7c0538eec91e548ac3e6dc41dd63f
zuweie <jojoe.wei@gmail.com>
2021-05-13 16:41:01 +0800

    support vector machines

--- src/machine_learning/support_vector_machines.c
+++ src/machine_learning/support_vector_machines.c
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-05-10 13:15:21
- * @LastEditTime: 2021-05-10 13:16:21
+ * @LastEditTime: 2021-05-13 12:48:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /boring-code/src/machine_learning/svm.c
  */
-#include "svm.h"
+#include "support_vector_machines.h"
--- src/machine_learning/support_vector_machines.h
+++ src/machine_learning/support_vector_machines.h
@@ -1,12 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2021-05-10 13:15:30
- * @LastEditTime: 2021-05-10 23:28:07
+ * @LastEditTime: 2021-05-13 16:40:13
  * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @Description: 软间隔支持向量机的实现
  * @FilePath: /boring-code/src/machine_learning/svm.h
  */
 #ifndef _SUPPORT_VECTOR_MACHINES_H_
 #define _SUPPORT_VECTOR_MACHINES_H_
+
+#include "vtype/vfloat_type.h"
+
+typedef struct _u_array u_array_t;
+
+int support_vector_machine_train(u_array_t* X, u_array_t* y, u_array_t* w);
 
 #endif
