4a012942fbafeef3010ed864e638c11ae04c200e
zuweie <jojoe.wei@gmail.com>
2021-11-15 16:50:34 +0800

    finish the svm ml 1.0

---
+++ src/machine_learning/neural_network.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-15 16:45:08
+ * @LastEditTime: 2021-11-15 16:45:09
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /boring-code/src/machine_learning/neural_network.c
+ */
---
+++ src/machine_learning/neural_network.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-15 16:45:20
+ * @LastEditTime: 2021-11-15 16:47:31
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /boring-code/src/machine_learning/neural_network.h
+ */
+#ifndef __NEURAL_NETWORK_H__
+#define __NEURAL_NETWORK_H__
+
+#endif
