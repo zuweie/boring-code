53845529f6a01b06d5d45bb37a594e11d7ed2ac0
zuweie <jojoe.wei@gmail.com>
2022-02-03 14:26:54 +0800

    decision tree

--- src/machine_learning/decision_tree.c
+++ src/machine_learning/decision_tree.c
@@ -1,9 +1,34 @@
 /*
  * @Author: your name
  * @Date: 2022-01-17 11:00:34
- * @LastEditTime: 2022-01-17 11:02:26
+ * @LastEditTime: 2022-02-03 14:22:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /boring-code/src/machine_learning/desicion_tree.c
  */
+#include "ultra_array/ultra_array.h"
 #include "decision_tree.h"
+
+int find_best_split(u_array_t* _X, int* attr_list) {
+     
+}
+
+int cart_training(u_array_t* X, u_array_t* Y, car_tree_t* tree, unsigned char is_classifier)
+{
+    // TODO:
+    // 1 检查维数是否正确。
+    if (UA_shape_axis(X, 0) != UA_length(Y)) return -1;
+    int i, j, k;
+    int attr_list_num = UA_shape_axis(X, 1);
+    int attr_list[attr_list_num];
+    // 
+    for (i=0; i<attr_list_num; ++i) {
+        attr_list[i] = i;
+    }
+
+    int best_split_id = find_best_split(X, attr_list);
+
+    return 0;
+}
+int cart_finalize(car_tree_t* tree);
+int cart_predict(u_array_t* sample, car_tree_t* tree);
--- src/machine_learning/decision_tree.h
+++ src/machine_learning/decision_tree.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-17 11:00:41
- * @LastEditTime: 2022-01-17 11:03:52
+ * @LastEditTime: 2022-02-03 13:59:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /boring-code/src/machine_learning/desicion_tree.h
@@ -9,6 +9,26 @@
 #ifndef __DECISION_TREE_H__
 #define __DECISION_TREE_H__
 
+#include "ultra_array/ultra_array.h"
 
+typedef struct _cart_node_t cart_node_t;
+
+struct _cart_node_t{
+    float gini;
+    float cr;
+    int attribe_id;
+    cart_node_t* parent;
+    cart_node_t* lef;
+    cart_node_t* right;
+} cart_node_t;
+
+typedef struct _car_tree {
+    cart_node_t* node;
+    unsigned char is_classifier;
+} car_tree_t;
+
+int cart_training(u_array_t* X, u_array_t* Y, car_tree_t* tree, unsigned char is_classifier);
+int cart_finalize(car_tree_t* car_tree);
+int cart_predict(u_array_t* sample, car_tree_t* car_tree);
 
 #endif
