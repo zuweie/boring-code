cbbfd5b44969c3f3ea2810970035d6b88ae73d64
zuweie <jojoe.wei@gmail.com>
2022-02-14 14:23:44 +0800

    to be continue ...

--- src/machine_learning/decision_tree.c
+++ src/machine_learning/decision_tree.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-17 11:00:34
- * @LastEditTime: 2022-02-03 14:22:29
+ * @LastEditTime: 2022-02-14 14:22:02
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /boring-code/src/machine_learning/desicion_tree.c
@@ -9,26 +9,21 @@
 #include "ultra_array/ultra_array.h"
 #include "decision_tree.h"
 
-int find_best_split(u_array_t* _X, int* attr_list) {
-     
+car_tree_t* 
+car_tree_training(u_array_t* X, u_array_t* Y, unsigned char is_classifier)
+{
+    // TO be continue ...
+    return NULL;
 }
 
-int cart_training(u_array_t* X, u_array_t* Y, car_tree_t* tree, unsigned char is_classifier)
+int car_tree_finalize(car_tree_t* car_tree)
 {
-    // TODO:
-    // 1 检查维数是否正确。
-    if (UA_shape_axis(X, 0) != UA_length(Y)) return -1;
-    int i, j, k;
-    int attr_list_num = UA_shape_axis(X, 1);
-    int attr_list[attr_list_num];
-    // 
-    for (i=0; i<attr_list_num; ++i) {
-        attr_list[i] = i;
-    }
-
-    int best_split_id = find_best_split(X, attr_list);
-
+    // TO be continue ...
     return 0;
 }
-int cart_finalize(car_tree_t* tree);
-int cart_predict(u_array_t* sample, car_tree_t* tree);
+
+int car_tree_predict(u_array_t* sample, car_tree_t* car_tree)
+{
+    // TO be continue ...
+    return 0;
+}
--- src/machine_learning/decision_tree.h
+++ src/machine_learning/decision_tree.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-17 11:00:41
- * @LastEditTime: 2022-02-03 13:59:38
+ * @LastEditTime: 2022-02-14 12:53:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /boring-code/src/machine_learning/desicion_tree.h
@@ -11,24 +11,24 @@
 
 #include "ultra_array/ultra_array.h"
 
-typedef struct _cart_node_t cart_node_t;
+typedef struct _car_node_t car_node_t;
 
 struct _cart_node_t{
     float gini;
     float cr;
     int attribe_id;
-    cart_node_t* parent;
-    cart_node_t* lef;
-    cart_node_t* right;
-} cart_node_t;
+    car_node_t* parent;
+    car_node_t* lef;
+    car_node_t* right;
+} cart_node_t; 
 
 typedef struct _car_tree {
-    cart_node_t* node;
+    car_node_t* node;
     unsigned char is_classifier;
 } car_tree_t;
 
-int cart_training(u_array_t* X, u_array_t* Y, car_tree_t* tree, unsigned char is_classifier);
-int cart_finalize(car_tree_t* car_tree);
-int cart_predict(u_array_t* sample, car_tree_t* car_tree);
+car_tree_t* car_tree_training(u_array_t* X, u_array_t* Y, unsigned char is_classifier);
+int car_tree_finalize(car_tree_t* car_tree);
+int car_tree_predict(u_array_t* sample, car_tree_t* car_tree);
 
 #endif
