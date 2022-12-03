852b1348ad00cc0dcdc9804ade6672e7e91080f4
zuweie <jojoe.wei@gmail.com>
2022-12-03 11:25:18 +0800

    what the fuck sudoku

---
+++ src/leetcode/37.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: zuweie jojoe.wei@gmail.com
+ * @Date: 2022-12-01 11:12:42
+ * @LastEditors: zuweie jojoe.wei@gmail.com
+ * @LastEditTime: 2022-12-03 11:24:35
+ * @FilePath: /boring-code/src/leetcode/37.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+void solveSudoku(char** board, int boardSize, int* boardColSize) 
+{
+    
+}
