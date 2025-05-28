50ac9c408338f7851c89bf164b2fbc1e2f3241bc
zuweie <jojoe.wei@gmail.com>
2025-05-28 16:31:31 +0800

    what the fuck

--- src/deep_learning/compute_graph2/cg_znode.h
+++ src/deep_learning/compute_graph2/cg_znode.h
@@ -1,4 +1,16 @@
 #ifndef __CG_ZNODE_H__
 #define __CG_ZNODE_H__
 
+#include "cg_graph.h"
+#include "cg_tensor.h"
+#include "opt/cg_opt_base.h"
+typedef struct cg_znode {
+    
+    cg_vertex_t* vertex;
+    cg_tensor_t* payload;
+    cg_tensor_t* gradient;
+    cg_opt_base* opt;
+
+} cg_znode_t;
+
 #endif
---
+++ src/deep_learning/compute_graph2/opt/cg_opt_plus.h
@@ -0,0 +1,4 @@
+#ifndef __CG_OPT_PLUS_H__
+#define __CG_OPT_PLUS_H__
+
+#endif
