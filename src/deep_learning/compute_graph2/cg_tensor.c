f2c96dadebb8fbeb31c018b67c5be245c39ca75f
zuweie <jojoe.wei@gmail.com>
2025-05-29 18:20:35 +0800

    what the fuck

--- src/deep_learning/compute_graph2/cg_tensor.c
+++ src/deep_learning/compute_graph2/cg_tensor.c
@@ -2,7 +2,7 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2025-05-24 09:57:39
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2025-05-29 16:07:11
+ * @LastEditTime: 2025-05-29 18:01:30
  * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_tensor.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,3 +37,11 @@ int cg_tensor_cycle(cg_tensor_t* thiz)
     free(thiz);
     return 0;
 }
+
+int cg_tensor_slice(cg_tensor_t* thiz, ...)
+int cg_tensor_padding(cg_tensor_t* this, ...)
+
+int cg_tensor_dot(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
+int cg_tensor_sum(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
+int cg_tensor_subtract(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
+int cg_tensor_scale(cg_tensor_t*, float scale)
