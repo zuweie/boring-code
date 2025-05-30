a5928b36822619e32a1fac46644d0952d9482ae6
zuweie <jojoe.wei@gmail.com>
2025-05-30 12:29:01 +0800

    what the fuck

--- src/deep_learning/compute_graph2/cg_tensor.c
+++ src/deep_learning/compute_graph2/cg_tensor.c
@@ -2,44 +2,115 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2025-05-24 09:57:39
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2025-05-29 18:01:30
+ * @LastEditTime: 2025-05-30 12:28:19
  * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_tensor.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 #include "cg_debug.h"
 #include "cg_tensor.h"
 
-cg_tensor_t* cg_tensor_create(cg_allocator_t* alloc, int axes, ...)
+static float* __coordinate_router(cg_tensor_t* tensor, int axes, int coordinate[]) 
+{
+    float* dist = tensor->elems;
+    for (int i=0; i<axes; ++i) {
+        dist += coordinate[i] * TENSOR_STRIDE(tensor, i)
+    }
+    return dist, 
+}
+
+static int __reshape(cg_tensor_t* tensor, int axes, int dimensions[]) 
+{
+    unsigned int old_size = 0;
+    if (tensor->dimensions == NULL) { 
+        tensor->dimensions = (int*) malloc ((1+2*axes) * sizeof(int));
+    } else if (axes > TENSOR_AXES(tensor)) {
+        old_size = TENSOR_SIZE(tensor);
+        tensor->dimensions = (int*) realloc ((1+2*axes) * sizeof(int));
+    }
+
+    TENSOR_AXES(tensor) = axes;
+    // 更新维度信息。
+    for (int i=axes-1; i>=0 ; --i) {
+        TENSOR_DIMEN(tensor, i)  = dimensions[i];
+        TENSOR_STRIDE(tensor, i) = (i == axes-1 ? 1 : TENSOR_DIMEN(tensor, (i+1)) * TENSOR_STRIDE(tensor, (i+1)));
+    }
+
+    // 跟新 element 池子大小 
+    if (TENSOR_SIZE(tensor) > old_size) {
+
+        float* new_elemes = cg_alloc(tensor->allocator, TENSOR_SIZE(tensor));
+
+        // 如果原来旧有数据复制旧的，然后回收。
+        if (tensor->elems) {
+            memcpy(new_elemes, tensor->elems, old_size);
+            cg_recycle(tensor->allocator, tensor->elems);
+        }
+        tensor->elems = new_elemes;
+    }
+    
+    return 0;
+}
+cg_tensor_t* __create_tensor(cg_allocator_t* alloc, int axes, int dimension[])
 {
     cg_tensor_t* tensor = (cg_tensor_t*) malloc(sizeof(cg_tensor_t) + sizeof(int) * axes);
     tensor->allocator   = alloc;
-    tensor->axes        = axes;
+    tensor->dimensions  = NULL;
+    tensor->elems       = NULL;
+    __reshape(tensor, axes, dimension);
+    return tensor;
+}
+
+cg_tensor_t* cg_tensor_create(cg_allocator_t* alloc, int axes, ...)
+{
+    int dimensions[axes];
 
     va_list args;
     va_start(args, axes);
-    int elems_size = 1;
-
     for (int i=0; i<axes; ++i) {
-        tensor->dimens[i] = va_arg(args, int);
-        elems_size       *= tensor->dimens[i];
+        dimensions[i] = va_arg(args, int);
     }
-    tensor->elems = (float*)cg_alloc(alloc, elems_size * sizeof(float));
-    
     va_end(args);
-    return tensor;
+    
+    return __create_tensor(alloc, axes, dimensions);
 }
 
 int cg_tensor_cycle(cg_tensor_t* thiz)
 {
     cg_recycle(thiz->allocator, thiz->elems);
+    free(thiz->dimensions);
     free(thiz);
     return 0;
 }
 
-int cg_tensor_slice(cg_tensor_t* thiz, ...)
-int cg_tensor_padding(cg_tensor_t* this, ...)
+
+cg_tensor_t* cg_tensor_slice(cg_tensor_t* thiz, int slice_axes, ...)
+{
+    int slice_dimensions[ 2 * TENSOR_AXES(thiz) ];
+    int new_dimensions[TENSOR_AXES(thiz)];
+
+    va_list vargs;
+    va_start(vargs, slice_axes);
+    for (i=0; i<TENSOR_AXES(thiz); ++i) {
+        slice_dimensions[i*2]   = i<slice_axes? va_arg(args, int) : 0;
+        slice_dimensions[i*2+1] = i<slice_axes? va_arg(args, int) : TENSOR_DIMEN(this, i);
+        new_dimensions[i] = slice_dimensions[i*2+1] - slice_dimensions[i*2];
+    }
+    va_end(vargs);
+
+    cg_tensor_t* tensor = __create_tensor(thiz->alloc, TENSOR_AXES(this), new_dimensions);
+
+    // copy 数据
+     
+    return tensor;
+}
+
+cg_tensor_t* cg_tensor_padding(cg_tensor_t* this, int axes, ...);
+{
+
+}
 
 int cg_tensor_dot(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
 int cg_tensor_sum(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
--- src/deep_learning/compute_graph2/cg_tensor.h
+++ src/deep_learning/compute_graph2/cg_tensor.h
@@ -1,24 +1,37 @@
+/*
+ * @Author: zuweie jojoe.wei@gmail.com
+ * @Date: 2025-05-24 09:57:43
+ * @LastEditors: zuweie jojoe.wei@gmail.com
+ * @LastEditTime: 2025-05-30 11:44:09
+ * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_tensor.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE  
+ */
 #ifndef __CG_TENSOR_H__
 #define __CG_TENSOR_H__
 #include "cg_allocator.h"
 
+#define TENSOR_AXES(tensor)      ((tensor)->dimension[0])
+#define TENSOR_DIMEN(tensor, i)  ((tensor)->dimension[i+1])
+#define TENSOR_STRIDE(tensor, i) ((tensor)->dimension[TENSOR_AXES[(tensor)]+1+i])
+#define TENSOR_NUM(tensor)  (TENSOR_DIMEN(tensor, 0) * TENSOR_STRIDE(tensor, 0))
+
+#define TENSOR_SIZE(tensor) (TENSOR_NUM(tensor) * sizeof(float))
+
 typedef struct cg_tensor {
     cg_allocator_t* allocator;
-    int   axes;
-    float* elems;
-    int dimens[0];
+    float*          elems;
+    int*            dimensions;
 } cg_tensor_t;
 
 cg_tensor_t* cg_tensor_create(cg_allocator_t* alloc, int axes, ...);
 int cg_tensor_recycle(cg_tensor_t* thiz);
 
-int cg_tensor_slice(cg_tensor_t* thiz, ...)
-int cg_tensor_padding(cg_tensor_t* this, ...)
+cg_tensor_t* cg_tensor_slice(cg_tensor_t* thiz, int axes, ...);
+cg_tensor_t* cg_tensor_padding(cg_tensor_t* this, int axes, ...);
 
-int cg_tensor_dot(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
-int cg_tensor_sum(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
-int cg_tensor_subtract(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
-int cg_tensor_scale(cg_tensor_t*, float scale)
-
+int cg_tensor_dot(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2);
+int cg_tensor_sum(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2);
+int cg_tensor_subtract(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2);
+int cg_tensor_scale(cg_tensor_t*, float scale);
 
 #endif
