b59feb18b02a4528799229c7fad8fb0e3d19ce4c
zuweie <jojoe.wei@gmail.com>
2025-05-31 16:30:19 +0800

    doting

--- src/deep_learning/compute_graph2/cg_tensor.c
+++ src/deep_learning/compute_graph2/cg_tensor.c
@@ -2,7 +2,7 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2025-05-24 09:57:39
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2025-05-31 12:46:19
+ * @LastEditTime: 2025-05-31 16:19:12
  * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_tensor.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -156,10 +156,63 @@ static int __do_padding(const void* target_base, const int* target_dimens, const
     return 0;
 }
 
-static int __do_dot(void** target_base, int** target_dimens, const void* t1_base, const void* t1_dimens, const void* t2_base, const int* t2_dimens)
+static int __matrix_doting (void* dist, void* m1, int r1, int c1, void* m2, int r2, int c2)
 {
-    int axes = _D_AXES(t1_dimens);
+    memset(dist, 0, r1*c2*sizeof(float));
+    float* p_dist = dist;
+    float* p_m1   = m1;
+    float* p_m2   = m2;
+    int    k      = 0
+    for (int i=0; i<r1; ++i) {
+        for (int j=0; j<c2; ++j) {
+            p_dist[k++] = p_m1[i*c1+j] * p_m2[j*r2+i];
+        }
+    }
+    return 0;
+}
+
+static int __do_doting(void* target_base, int* target_dimens, const void* t1_base, const int* t1_dimens, const void* t2_base, const int* t2_dimens, int curr_axis, int target_coord[], int t1_coord[], int t2_coord[]) {
     
+    int axes = curr_axis + 1;
+
+    // 到了倒数第二维度。开始做矩阵乘法。
+    if (curr_axis == _D_AXES(target_dimens) - 2) {
+        void* dist = __coordinate_router(target_base, target_dimens, axes, target_coord);
+        void* m1   = __coordinate_router(t1_base, t1_dimens, axes, t1_coord);
+        void* m2   = __coordinate_router(t2_base, t2_dimens, axes, t2_coord);
+
+        return __matrix_doting(dist, m1, _D_DIMEN(t1_dimens, curr_axis), _D_DIMEN(t1_dimens, curr_axis+1), m2, _D_DIMEN(t2_dimens, curr_axis), _D_DIMEN(t2_dimens, curr_axis+1));
+
+    } else {
+        for (int i=0; i<_D_DIMEN(target_dimens); ++i) {
+            target_coord[curr_axis] = i;
+            t1_coord[curr_axis] = i < _D_DIMEN(t1_dimens, curr_axis) ? i : 0;
+            t2_coord[curr_axis] = i < _D_DIMEN(t2_dimens, curr_axis) ? i : 0;
+            return __do_doting(target_base, target_dimens, t1_base, t1_dimens, t2_base, t2_dimens, curr_axis+1, target_coord, t1_coord, t2_coord);
+        }
+    }
+}
+
+static int __dot(void** target_base, int** target_dimens, const void* t1_base, const int* t1_dimens, const void* t2_base, const int* t2_dimens, cg_allocator_t* alloc)
+{
+    // 计算内积后的形状。
+    int axes  = _D_AXES(t1_dimens);
+    int dimens[axes];
+    int target_coord[axes];
+    int t1_coord[axes];
+    int t2_coord[axes];
+
+    int i;
+    for (i=0; i<axes-2, ++i) {
+        dimens[i] = _D_DIMEN(t1_dimens, i) >= _D_DIMEN(t2_dimens, i)? _D_DIMEN(t1_dimens, i) : _D_DIMEN(t2_dimens, i);
+    }
+    dimens[axes-2] = _D_DIMEN(t1_dimens, axes-2);
+    dimens[axes-1] = _D_DIMEN(t2_dimens, axes-1);
+
+    // 首先改变形状。
+    __reshape(target_base, target_dimens, axes, dimens, alloc);
+
+    return __do_doting((*target_base), (*target_dimens), t1_base, t1_dimens, t2_base, t2_dimens, 0, target_coord, t1_coord, t2_coord);
 }
 
 cg_tensor_t* cg_tensor_create(cg_allocator_t* alloc, int axes, ...)
@@ -230,11 +283,49 @@ cg_tensor_t* cg_tensor_padding(cg_tensor_t* thiz, float fill, int padding_axes, 
 
 int cg_tensor_dot(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
 {
+    // 检查 t1, t2 维度是否能广播。
+    int axes1 = TENSOR_AXES(t1);
+    int axes2 = TENSOR_AXES(t2);
+    if (axes1 != axes2) {
+        CG_DEBUG("t1 axes %d not euq t2 axes %d\n", axes1, axes2);
+        return -1;
+    } 
+    for (int i=0; i<axes1; ++i) {
+        if (TENSOR_DIMEN(t1, i) != TENSOR_DIMEN(t2, i)) {
+            if (TENSOR_DIMEN(t1, i) != 1 && TENSOR_DIMEN(t2, i) != 1) {
+                CG_DEBUG("t1 %d axis`s dimens is %d, t2 %d axis`s dimens is %d\n, not match!", i, TENSOR_DIMEN(t1, i), i, TENSOR_DIMEN(t2, i));
+                return -1;
+            } 
+        }
+    }
 
+    return __dot(&r->elems, *r->dimensions, t1->elems, t1->dimensions, t2->elems, t2->dimensions, r->allocator);
 }
 int cg_tensor_sum(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
 {
+    int axes1 = TENSOR_AXES(t1);
+    int axes2 = TENSOR_AXES(t2);
+    if (axes1 != axes2) {
+        CG_DEBUG("t1 axes %d not euq t2 axes %d\n", axes1, axes2);
+        return -1;
+    } 
+    for (int i=0; i<axes1; ++i) {
+        if (TENSOR_DIMEN(t1, i) != TENSOR_DIMEN(t2, i) && TENSOR_DIMEN(t1, i) != TENSOR_DIMEN(r, i)) {
+            CG_DEBUG("t1 %d axis`s dimens is %d, t2 %d axis`s dimens is %d\n, not match!", i, TENSOR_DIMEN(t1, i), i, TENSOR_DIMEN(t2, i));
+            return -1;
+        }
+    }
 
+    // reshap the r
+    // TODO: do sum.
+    float* p_r = r->elems;
+    float* p_t1 = t1->elems;
+    float* p_t2 = t2->elems;
+    
+    for (int i=0; i<TENSOR_NUM(r); ++i) {
+        
+    }
+    return 0;
 }
 int cg_tensor_subtract(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
 {
@@ -242,5 +333,5 @@ int cg_tensor_subtract(cg_tensor_t* r, cg_tensor_t* t1, cg_tensor_t* t2)
 }
 int cg_tensor_scale(cg_tensor_t*, float scale)
 {
-
+    
 }
