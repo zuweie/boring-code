f0a3ceb7a690a2c5bd1eaa13ae91096b1be1ff7f
zuweie <jojoe.wei@gmail.com>
2026-02-21 23:26:39 +0800

    refactor compute graph2

--- src/deep_learning/compute_graph2/cg_calflow.c
+++ src/deep_learning/compute_graph2/cg_calflow.c
@@ -2,11 +2,12 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2026-02-19 15:08:47
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2026-02-20 15:49:33
+ * @LastEditTime: 2026-02-21 23:26:02
  * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_calflow.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include "cg_debug.h"
+#include "cg_hash.h"
 #include "cg_node.h"
 #include "cg_operand.h"
 #include "cg_operator.h"
@@ -35,11 +36,32 @@ static int __do_calculate(cg_node_t* znode, int version, cg_ref_t params)
     return 0;
 }
 
-static int __do_derivative(cg_node_t* znode, int version, cg_ref_t params)
+static int __distribute_tickets (cg_node_t* znode, cg_hash_t* tickets) 
 {
-    cg_operand_t* operand = (cg_operand_t*) znode;
-    if ()
-    // 遍历
+    cg_node_t* operator_node = cg_list_get(znode->vertex.in, 0);
+    
+    cg_list_node_t* first   = CG_LIST_TOP(operator_node->vertex.in);
+
+    while (first != CG_LIST_HEAD(operator_node->vertex.in))
+    {
+        cg_node_t* operand = first->ref;
+        cg_ticket_t* new_ticket = cg_ticket_create(operator_node->vertex.id);
+
+        if (cg_hash_has(tickets, operand->vertex.id)) {
+            // 插在屁股后面，
+            cg_ticket_t* ticket = cg_hash_get(tickets, operand->vertex.id);
+            ticket->next = ticket;
+        } else {
+            // 生成一个新的，放在里面去。
+            cg_hash_set(tickets, operand->vertex.id, new_ticket);
+        }
+        /* code */
+    }
+    
+}
+
+static int __do_derivative(cg_node_t* znode, int version, cg_hash_t* tickers, cg_ref_t params)
+{
     return 0;
 }
 
@@ -51,6 +73,22 @@ int cg_calculate(cg_node_t* znode, int version, cg_ref_t params)
     return -1;
 }
 
+
+/**
+ * @brief 这里我发明了一种 ticket 算法。
+ * 第一步我从一个 operand A 的节点出发，使用深度优先算法，遍历其地下所有的节点。经过一个 operand X 节点，便给这个 operand X 节点派发，一张 ticket。
+ * 若果有多次经过这个 operand X 节点，它将获得多个 ticket。
+ * 第二步，开始正式的做 derivative 的时候，我们会将遇到的要计算 gradient 的 operand 的时候，我们将检查他的上级节点是否与 ticket 的中的 handle 是否一致。
+ * 如果不一致，那么说明这个计算图的拓扑结果是错误的。当完成了这个 derivative 后，对应的 ticket 删除，然后更新数据节点的 version。那么这个节点的 derivate 
+ * 工作就完成了。然后将这个节点往下一级的尚未对其子节点的节点重复上述动作。
+ * 
+ * 简化版，可以将 ticket 换成计数，但是无法验证上家是否一致。
+ * 
+ * @param znode 
+ * @param version 
+ * @param params 
+ * @return int 
+ */
 int cg_derivative(cg_node_t* znode, int version, cg_ref_t params)
 {
     if (znode->node_type == e_operand && cg_list_size(znode->vertex.in) <= 1) {
--- src/deep_learning/compute_graph2/cg_node.h
+++ src/deep_learning/compute_graph2/cg_node.h
@@ -15,6 +15,14 @@ typedef struct cg_node {
     cg_node_type_t node_type;
 } cg_node_t;
 
+typedef struct cg_ticket {
+
+    char node_id[64];
+    int used;
+    struct cg_ticket* next;
+
+} cg_ticket_t;
+
 static inline int cg_node_init(cg_node_t* thiz, const char* id, cg_node_type_t node_type) 
 {
     thiz->node_type = node_type;
@@ -30,6 +38,46 @@ static inline int cg_node_reset(cg_node_t* thiz)
 {
     cg_list_recycle(thiz->vertex.in, NULL);
     cg_list_recycle(thiz->vertex.out, NULL);
+    return 0;
+}
+
+static inline cg_ticket_t* cg_ticket_create(const char* id) 
+{
+    cg_ticket_t* ticket = (cg_ticket_t*) malloc (sizeof(cg_ticket_t));
+    strcpy(ticket->node_id, id);
+    ticket->used = 0;
+    ticket->next = NULL;
+    return ticket;
+}
+
+static inline int cg_ticket_insert(cg_ticket_t** next, cg_ticket_t* ticket) 
+{
+    *next = ticket;
+    return 0;
+}
+
+static inline int cg_ticket_recycle(cg_ticket_t* ticket_list)
+{
+    cg_ticket_t* del;
+    while( ticket_list ) {
+        del         = ticket_list;
+        ticket_list = del->next;
+        free(del);
+    }
+    return 0;
+}
+
+static inline int cg_ticket_check(cg_ticket_t* ticket_list) 
+{
+    if (ticket_list) {
+        int finished;
+        cg_ticket_t* first = ticket_list;
+        while (first && (finished = first->used)) {
+            first = first->next;
+        }
+        return finished;
+    }
+    return -1;
 }
 
 #endif
