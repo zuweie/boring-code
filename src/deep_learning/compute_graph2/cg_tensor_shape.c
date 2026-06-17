a6a0e924c9f9f1e2d8dbd79fc6c75f5ae5965dbc
zuweie <jojoe.wei@gmail.com>
2026-06-18 01:49:09 +0800

    refactor shape

---
+++ src/deep_learning/compute_graph2/cg_tensor_shape.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include "cg_debug.h"
+#include "cg_tensor_shape.h"
+
+int cg_tensor_shape_create(cg_tensor_axis_t** first, int input_axes, int input_dimens[])
+{
+    *first = NULL;
+    cg_tensor_axis_t* axis0 = (cg_tensor_axis_t*) malloc (sizeof(cg_tensor_axis_t));
+    *axis0 = (cg_tensor_axis_t) {
+        .axes   = 0,
+        .dimens = 1,
+        .stride = 1,
+        .next   = axis0
+    }
+    *first = axis0;
+
+    int axes_count = input_axes;
+    for (int i=0; i<input_axes; ++i) {
+        axis0 = (cg_tensor_axis_t*) malloc (sizeof(cg_tensor_axis_t));
+        axis0 = (cg_tensor_axis_t) {
+            .axes   = axes_count--,
+            .dimens = input_dimens[i],
+            .stride = (*first)->stride * (*first)->dimens,
+            .next   = *first
+        }
+        *first = axis0;
+    }
+    return 0;
+}
+
+int cg_tensor_shape_recycle(cg_tensor_axis_t* first)
+{
+    cg_tensor_axis_t* del = first;
+
+    while (del->axes) {
+        
+        first = del->next;
+        free(del);
+        del   = first;
+    }
+    free(del);
+    return 0;
+}
+
+int cg_tensor_shape_split_out(cg_tensor_axis_t** axis, int* cut_out, int coord_axes, int coordinate[])
+{
+    int ret                 = 0;
+    *cut_out                = 0;
+    cg_tensor_axis_t* first = *axis;
+
+    for (int i=0; i<coord_axes; ++i) {
+        if (coordinate[i] >=0 && coordinate[i] < first->dimens) {
+            
+            *cut_out += coordinate[i] * first->stride;
+            first     = first->next;
+
+        } else {
+            *cut_out = -1;
+            return -1;
+        }
+    }
+    *axis = first;
+    return 0;
+}
+
+int cg_tensor_shape_display(cg_tensor_axis_t* axis)
+{
+    if (axis->axes == 0) {
+        printf("\n axes: %d, dimens: %d, stride: %d\n" axis->axes, axis->dimens, axis->stride);
+    } else {
+        cg_tensor_axis_t* first = axis;
+        printf("\n axes: %d\n", first->axes);
+        printf("\n dimens: ");
+        while (first->axes) {
+            printf("%d, ", first->dimens);
+            first = first->next;
+        }
+        first = axis;
+        printf("\n stride: ");
+        while (first->axes) {
+            printf("%d, ", first->stride);
+            first = first->next;
+        }
+        printf("\n");
+        
+    }
+    
+}
--- src/deep_learning/compute_graph2/cg_tensor_shape.h
+++ src/deep_learning/compute_graph2/cg_tensor_shape.h
@@ -2,167 +2,25 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2026-06-07 22:30:37
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2026-06-14 22:04:12
+ * @LastEditTime: 2026-06-18 01:45:03
  * @FilePath: /boring-code/src/deep_learning/compute_graph2/cg_tensor_dimensions.h
  * @Description: 这是一个统一处理 tensor 的 shape 的处理函数。
  */
 #ifndef __CG_TENSRO_SHAPE_H__
 #define __CG_TENSRO_SHAPE_H__
-#include <stdlib.h>
-#include "cg_debug.h"
 
+typedef struct cg_tensor_axis {
 
-static inline int* cg_tensor_shape_create(int input_axes, int input_dimens[]) 
-{
-    int ret = 0;
-    int* dimension = NULL;
-    int i,j;
-    if (input_axes > 0) {
+    int axes;
+    int dimens;
+    int stride;
+    struct cg_tensor_axis* next;
 
-        for (i=0; i<input_axes; ++i) {
-            if (input_dimens[i] <=0) {
-                ret = -1;
-                CG_DEBUG("Error <%d@%s>: invalid input_dimens[%d](%d)\n", __LINE__, __FILE__, i, input_dimens[i]);
-                break;
-            }
-        }
+} cg_tensor_axis_t;
 
-        if (ret == 0) {
+int cg_tensor_shape_create(cg_tensor_axis_t** first, int input_axes, int input_dimens[]);
+int cg_tensor_shape_recycle(cg_tensor_axis_t* first);
 
-            int axes  = input_axes + 1;
-
-            dimension         = (int*) malloc ( (axes*2+1) * sizeof (int) );
-            dimension[0]      = axes;
-            dimension[axes]   = 1;
-            dimension[axes*2] = 1;
-
-            for (i=axes-1, j=input_axes-1; i>=1; --i, --j) {
-                dimension[i]      = input_dimens[j];
-                dimension[axes+i] = dimension[axes+i+1] * dimension[i+1];
-            }
-        }
-
-    } else {
-        CG_DEBUG("Error <%d@%s>: invalid input_axes %d\n", __LINE__, __FILE__, input_axes);
-    }
-    
-    return dimension;
-}
-
-static inline int cg_tensor_shape_recycle(int* cg_shape) 
-{
-    free(cg_shape);
-    return 0;
-}
-
-static inline int cg_tensor_shape_axes(int* cg_shape, int axis_cursor)
-{
-    if (axis_cursor >= 0 && axis_cursor < cg_shape[0]) {
-        return axis_cursor == cg_shape[0] - 1 ? 1 : cg_shape[0] - axis_cursor - 1;
-    }
-
-    if (axis_cursor < 0)
-        CG_DEBUG("Error <%d@%s>: axis_cursor(%d) is invalid\n", __LINE__, __FILE__, axis_cursor);
-    
-    if (axis_cursor >= cg_shape[0])
-        CG_DEBUG("Error <%d@%s>: axis_cursor(%d) is out of axes(%d) \n", __LINE__, __FILE__, axis_cursor, cg_shape[0]);
-
-    return -1;
-}
-
-static inline int cg_tensor_shape_dimen(int* cg_shape, int axis_cursor, int axis_offset) 
-{   
-    if (axis_cursor >=0 && axis_offset >=0 && axis_cursor + axis_offset < cg_shape[0]) {
-        return cg_shape[axis_cursor + axis_offset + 1]; 
-    }
-    
-    if (axis_cursor < 0)
-        CG_DEBUG("Error <%d@%s>: axis_cursor(%d) is invalid\n", __LINE__, __FILE__, axis_cursor);
-
-    if (axis_cursor + axis_offset >= cg_shape[0])
-        CG_DEBUG(\
-            "Error <%d@%s>: explore scope(axis_cursor(%d) + axis_offset(%d) is out of axes(%d))\n", \
-            __LINE__, __FILE__, axis_cursor, axis_offset, cg_shape[0]\
-        );
-   return -1;
-}
-
-static inline int cg_tensor_shape_stride(int* cg_shape, int axis_cursor, int axis_offset) 
-{
-    if (axis_cursor >=0 && axis_offset >=0 && axis_cursor + axis_offset < cg_shape[0]) 
-        return cg_shape[ cg_shape[0] + axis_cursor + axis_offset + 1 ];
-    
-    if (axis_cursor < 0)
-        CG_DEBUG("Error <%d@%s>: axis_cursor(%d) is invalid\n", __LINE__, __FILE__, axis_cursor);
-
-    if (axis_cursor + axis_offset >= cg_shape[0])
-        CG_DEBUG(\
-            "Error <%d@%s>: explore scope(axis_cursor(%d) + axis_offset(%d) is out of axes(%d))\n", \
-            __LINE__, __FILE__, axis_cursor, axis_offset, cg_shape[0]\
-        );
-
-    return -1;
-}
-
-static inline int cg_tensor_shape_split_out(int* cg_shape, int* axis_cursor, int* cut_out, int coord_axes, int coord[]) 
-{
-    // list all the ivalid case.
-
-    if (*axis_cursor <0 || *axis_cursor >= cg_shape[0]) {
-        CG_DEBUG("Error <%d@%s>: axis_cursor(%d) < 0 or axis_cursor(%d) >= axes(%d)>\n", __LINE__, __FILE__, *axis_cursor, *axis_cursor, cg_shape[0]);
-        return -1;
-    }
-
-    if (coord_axes <= 0) {
-        CG_DEBUG("Error <%d@%s>: coord_axes(%d) is invalid\n", __LINE__, __FILE__, coord_axes);
-        return -1;
-    }
-
-    if (*axis_cursor + coord_axes > cg_shape[0])
-    {
-        CG_DEBUG("Error <%d@%s>: coord_axes(%d) out of scope \n", __LINE__, __FILE__, coord_axes);
-        return -1;
-    }
-
-    for (int i=0; i<coord_axes; ++i) {
-
-        
-        if ( coord[i] < 0 || coord[i] >= ) {
-            CG_DEBUG("Error <%d@%s>: coord[%d](%d) is out scope of axis(%d)(%d)\n",  \
-                __LINE__, __FILE__, i, coord[i], i,  \
-                
-            );
-            return -1
-        }
-    }
-
-    // start to do the split job
-    *cut_out = 0;
-    
-    for (int i=0; i<coord_axes; ++i) {
-        *cut_out += coord[i] * cg_tensor_shape_stride(cg_shape, *axis_cursor, i);
-    }
-
-    *axis_cursor += coord_axes;
-    
-    if (*axis_cursor == )
-
-    return -1;
-    
-}
-
-static inline int cg_tensor_shape_inspact(int* cg_shape, int axis_cursor) 
-{
-    int axes = cg_tensor_shape_axes(cg_shape, axis_cursor);
-    printf("\naxes: %d\n", axes);
-    printf("dimens: ");
-    for (int i=0; i<axes; ++i) {
-        printf("%d, ", cg_tensor_shape_dimen(cg_shape, axis_cursor, i));
-    }
-    printf("\nstride: ");
-    for (int i=0; i<axes; ++i) {
-        printf("%d, ", cg_tensor_shape_stride(cg_shape, axis_cursor, i));
-    }
-    printf("\n");
-}
+int cg_tensor_shape_split_out(cg_tensor_axis_t** axis, int* cut_out, int coord_axes, int coordinate[]);
+int cg_tensor_shape_display(cg_tensor_axis_t*);
 #endif
