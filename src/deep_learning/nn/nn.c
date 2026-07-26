a7506eb6518ae28f087980c49ddb98bb0659a16f
zuweie <jojoe.wei@gmail.com>
2026-07-26 17:43:34 +0800

    refactor calculation flow

--- src/deep_learning/nn/nn.c
+++ src/deep_learning/nn/nn.c
@@ -2,18 +2,21 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2026-03-14 11:35:50
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2026-07-26 14:37:25
+ * @LastEditTime: 2026-07-26 17:42:20
  * @FilePath: /boring-code/src/deep_learning/nn/nn.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <float.h>
+
 #include "deep_learning/compute_graph2/cg_allocator.h"
 #include "deep_learning/compute_graph2/cg_list.h"
 #include "deep_learning/compute_graph2/cg_tensor.h"
 #include "deep_learning/compute_graph2/cg_graph.h"
 #include "deep_learning/compute_graph2/cg_tensor_shape.h"
 #include "deep_learning/compute_graph2/cg_debug.h"
+#include "deep_learning/compute_graph2/cg_calculate_flow.h"
 
 #include "deep_learning/nnalloc/nn_alloc.h"
 #include "deep_learning/nn_operand/nn_operand.h"
@@ -49,22 +52,14 @@ static int __recycle_operator(cg_operator_t* operator)
 int nn_init(nn_t* nn, int batch, int max_iter, int err_stable, float epsilon, float alpah)
 {
     *nn = (nn_t) {
-        .batch    = batch,
-        .max_iter = max_iter,
-        .epsilon  = epsilon,
-        .alpha    = alpah,
-        .err_stable  = err_stable,
-        .znode_count = 0,
-
+        .znode_count  = 0,
         .operands     = cg_list_create(),
         .operators    = cg_list_create()
     };
     cg_allocator_init(&nn->alloc);
     Set_alloc(&nn->alloc);
-    
     return 0;
 }
-
 
 int nn_reset(nn_t* nn)
 {
@@ -74,7 +69,13 @@ int nn_reset(nn_t* nn)
     cg_list_recycle(nn->operands, __recycle_operand);
     cg_list_recycle(nn->operators, __recycle_operator);
 
-    free(nn->alloc);
+    //free(nn->alloc);
+    return 0;
+}
+
+int nn_set_train_package(nn_t* nn, nn_train_package_t* package)
+{
+    nn->train_package = package;
     return 0;
 }
 
@@ -107,6 +108,7 @@ nn_operand_t* nn_relu(nn_t* nn, nn_operand_t* _Input)
     relu_opt_t* relu_opt = relu_opt_create(__gen_id(nn, "relu_opt"), _Input);
     nn_operand_t* z      = nn_operand_create(__gen_id(nn, "_Z"), SHAPE_DIMENS(_Input->x->shape, 0), SHAPE_DIMENS(_Input->x->shape, 1));
 
+    cg_graph_link(_Input, relu_opt);
     cg_graph_link(relu_opt, z);
 
     // 将生成的节点收集起来，统一销毁。
@@ -120,6 +122,9 @@ nn_operand_t* nn_mse_loss(nn_t* nn, nn_operand_t* _Input, nn_operand_t* labels)
 {
     mse_opt_t* mse_opt = mse_opt_create(__gen_id(nn, "mse_loss_opt"), _Input, labels);
     nn_operand_t* z    = nn_operand_create(__gen_id(nn, "__Mse_loss"), 1, 1);
+
+    cg_graph_link(_Input, mse_opt);
+    cg_graph_link(labels, mse_opt);
     cg_graph_link(mse_opt, z);
 
     cg_list_push(nn->operands, z);
@@ -132,6 +137,9 @@ nn_operand_t* nn_crossentropy(nn_t* nn, nn_operand_t* _Input, nn_operand_t* labe
 {
     crossentropy_opt_t* cross_opt = crossentropy_opt_create(__gen_id(nn, "cross_opt"), _Input, labels);
     nn_operand_t* z               = nn_operand_create(__gen_id(nn, "__Cross_loss"), 1, 1);
+
+    cg_graph_link(_Input, cross_opt);
+    cg_graph_link(labels, cross_opt);
     cg_graph_link(cross_opt, z);
 
     cg_list_push(nn->operands, z);
@@ -143,6 +151,8 @@ nn_operand_t* nn_softmax(nn_t* nn, nn_operand_t* _Input)
 {
     softmax_opt_t* soft_opt = softmax_opt_create(__gen_id(nn, "softmax_opt"), _Input);
     nn_operand_t* z         = nn_operand_create(__gen_id(nn, "_Input"), SHAPE_DIMENS(_Input->x->shape, 0), SHAPE_DIMENS(_Input->x->shape, 1));
+
+    cg_graph_link(_Input, soft_opt);
     cg_graph_link(soft_opt, z);
 
     cg_list_push(nn->operands, z);
@@ -150,17 +160,28 @@ nn_operand_t* nn_softmax(nn_t* nn, nn_operand_t* _Input)
     return z;
 }
 
-int nn_forward(nn_t* nn)
+
+int nn_fit(nn_t* nn)
 {
-    
+
+    if (nn->train_package) {
+
+        int termin = 0;
+        do {
+            nn->train_package->prepare(nn);
+            cg_calculate_flow(nn->loss);
+            termin = nn->train_package->termin(nn);
+            if (!termin) {
+                // do backward propagetion 
+                cg_derivative_flow(nn->loss);
+            }
+        } while (termin);
+    } 
+    CG_DEBUG("Error <%d@%s>: nn does not have training package\n", __LINE__, __FILE__);
+    return -1;
 }
 
-int nn_backward(nn_t* nn)
+int nn_predict(nn_t* nn, cg_tensor_t* _Input)
 {
     
-}
-
-int nn_train(nn_t* nn)
-{
-
 }
--- src/deep_learning/nn/nn.h
+++ src/deep_learning/nn/nn.h
@@ -2,7 +2,7 @@
  * @Author: zuweie jojoe.wei@gmail.com
  * @Date: 2026-03-14 11:35:43
  * @LastEditors: zuweie jojoe.wei@gmail.com
- * @LastEditTime: 2026-07-26 14:37:12
+ * @LastEditTime: 2026-07-26 17:36:50
  * @FilePath: /boring-code/src/deep_learning/nn/nn.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,19 +13,11 @@
 
 typedef struct cg_allocator cg_allocator_t;
 typedef struct cg_tensor    cg_tensor_t;
+typedef struct nn           nn_t;
+typedef struct nn_train_package nn_train_package_t;
 
-typedef struct nn {
-
-    int    batch;
-    int    max_iter;
-    int    err_stable;
-    int    znode_count;
-
-    float  epsilon;
-    float  alpha;
-
-    cg_allocator_t alloc;
-
+struct nn {
+    cg_allocator_t  alloc;
     nn_operand_t*   labels;
     nn_operand_t*   output;
     nn_operand_t*   input;
@@ -33,14 +25,31 @@ typedef struct nn {
 
     cg_list_t*      operands;
     cg_list_t*      operators;
+    int             znode_count;
 
-    // use to build compute graph 
-    char            temp_id[64];
+    nn_train_package_t* train_package;
+};
 
-} nn_t;
+struct nn_train_package {
 
-int nn_init(nn_t* nn, int batch, int max_iter, int err_stable, float epsilon, float alpah);
+    int    batch;
+    int    max_iter;
+    int    err_stable;
+
+    float  epsilon;
+    float  alpha;
+
+    cg_tensor_t* train_datas;
+    cg_tensor_t* labels;
+    
+    int(*prepare)(nn_t* nn);
+    int(*termin) (nn_t* nn);
+}
+
+
+int nn_init(nn_t* nn);
 int nn_reset(nn_t* nn);
+int nn_set_train_package(nn_t* nn, nn_train_package_t* package);
 nn_operand_t* nn_linear(nn_t* nn, int out_dimens, nn_operand_t* _Input);
 nn_operand_t* nn_relu(nn_t* nn, nn_operand_t* _Input);
 nn_operand_t* nn_mse_loss(nn_t* nn, nn_operand_t* _Input, nn_operand_t* labels);
